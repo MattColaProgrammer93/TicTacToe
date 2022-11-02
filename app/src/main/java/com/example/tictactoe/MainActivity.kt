82b801996a39ddae97791b6f6117827d6b62b1cd
MattColaProgrammer93 <mattchoque115@gmail.com>
2022-11-02 09:42:41 -0700

    Added checkingForWinner which checks if current player mets winning condition, added winnerMet to declare winner, and changed the code in the tile's setOnClickListener

--- app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -27,71 +27,138 @@ class MainActivity : AppCompatActivity() {
         // New game
         val newGame = findViewById<Button>(R.id.newGame)
 
+        // Winner?
+        var winnerCheck = false
+
         // The tiles from 1-9
         firstTile.setOnClickListener {
-            if (firstTile.getText() == "") {
+            if (firstTile.getText() == "" && !winnerCheck) {
                 tileClicked(firstTile, playerTurn)
-                playerTurn.setText(changePlayerTurn(playerTurn))
+                winnerCheck = checkingForWinner(firstTile, secondTile, thirdTile, fourthTile, fifthTile,
+                    sixthTile, seventhTile, eighthTile, ninthTile)
+                if (winnerCheck){
+                    playerTurn.setText(winnerMet(playerTurn))
+                }
+                else if (!winnerCheck){
+                    playerTurn.setText(changePlayerTurn(playerTurn))
+                }
             }
         }
 
         secondTile.setOnClickListener {
-            if (secondTile.getText() == "") {
+            if (secondTile.getText() == "" && !winnerCheck) {
                 tileClicked(secondTile, playerTurn)
-                playerTurn.setText(changePlayerTurn(playerTurn))
+                winnerCheck = checkingForWinner(firstTile, secondTile, thirdTile, fourthTile, fifthTile,
+                    sixthTile, seventhTile, eighthTile, ninthTile)
+                if (winnerCheck){
+                    playerTurn.setText(winnerMet(playerTurn))
+                }
+                else if (!winnerCheck){
+                    playerTurn.setText(changePlayerTurn(playerTurn))
+                }
             }
         }
 
         thirdTile.setOnClickListener {
-            if (thirdTile.getText() == "") {
+            if (thirdTile.getText() == "" && !winnerCheck) {
                 tileClicked(thirdTile, playerTurn)
-                playerTurn.setText(changePlayerTurn(playerTurn))
+                winnerCheck = checkingForWinner(firstTile, secondTile, thirdTile, fourthTile, fifthTile,
+                    sixthTile, seventhTile, eighthTile, ninthTile)
+                if (winnerCheck){
+                    playerTurn.setText(winnerMet(playerTurn))
+                }
+                else if (!winnerCheck){
+                    playerTurn.setText(changePlayerTurn(playerTurn))
+                }
             }
         }
 
         fourthTile.setOnClickListener {
-            if (fourthTile.getText() == "") {
+            if (fourthTile.getText() == "" && !winnerCheck) {
                 tileClicked(fourthTile, playerTurn)
-                playerTurn.setText(changePlayerTurn(playerTurn))
+                winnerCheck = checkingForWinner(firstTile, secondTile, thirdTile, fourthTile, fifthTile,
+                    sixthTile, seventhTile, eighthTile, ninthTile)
+                if (winnerCheck){
+                    playerTurn.setText(winnerMet(playerTurn))
+                }
+                else if (!winnerCheck){
+                    playerTurn.setText(changePlayerTurn(playerTurn))
+                }
             }
         }
 
         fifthTile.setOnClickListener {
-            if (fifthTile.getText() == "") {
+            if (fifthTile.getText() == "" && !winnerCheck) {
                 tileClicked(fifthTile, playerTurn)
-                playerTurn.setText(changePlayerTurn(playerTurn))
+                winnerCheck = checkingForWinner(firstTile, secondTile, thirdTile, fourthTile, fifthTile,
+                    sixthTile, seventhTile, eighthTile, ninthTile)
+                if (winnerCheck){
+                    playerTurn.setText(winnerMet(playerTurn))
+                }
+                else if (!winnerCheck){
+                    playerTurn.setText(changePlayerTurn(playerTurn))
+                }
             }
         }
 
         sixthTile.setOnClickListener {
-            if (sixthTile.getText() == "") {
+            if (sixthTile.getText() == "" && !winnerCheck) {
                 tileClicked(sixthTile, playerTurn)
-                playerTurn.setText(changePlayerTurn(playerTurn))
+                winnerCheck = checkingForWinner(firstTile, secondTile, thirdTile, fourthTile, fifthTile,
+                    sixthTile, seventhTile, eighthTile, ninthTile)
+                if (winnerCheck){
+                    playerTurn.setText(winnerMet(playerTurn))
+                }
+                else if (!winnerCheck){
+                    playerTurn.setText(changePlayerTurn(playerTurn))
+                }
             }
         }
 
         seventhTile.setOnClickListener {
-            if (seventhTile.getText() == "") {
+            if (seventhTile.getText() == "" && !winnerCheck) {
                 tileClicked(seventhTile, playerTurn)
-                playerTurn.setText(changePlayerTurn(playerTurn))
+                winnerCheck = checkingForWinner(firstTile, secondTile, thirdTile, fourthTile, fifthTile,
+                    sixthTile, seventhTile, eighthTile, ninthTile)
+                if (winnerCheck){
+                    playerTurn.setText(winnerMet(playerTurn))
+                }
+                else if (!winnerCheck){
+                    playerTurn.setText(changePlayerTurn(playerTurn))
+                }
             }
         }
 
         eighthTile.setOnClickListener {
-            if (eighthTile.getText() == "") {
+            if (eighthTile.getText() == "" && !winnerCheck) {
                 tileClicked(eighthTile, playerTurn)
-                playerTurn.setText(changePlayerTurn(playerTurn))
+                winnerCheck = checkingForWinner(firstTile, secondTile, thirdTile, fourthTile, fifthTile,
+                    sixthTile, seventhTile, eighthTile, ninthTile)
+                if (winnerCheck){
+                    playerTurn.setText(winnerMet(playerTurn))
+                }
+                else if (!winnerCheck){
+                    playerTurn.setText(changePlayerTurn(playerTurn))
+                }
             }
         }
 
         ninthTile.setOnClickListener {
-            if (ninthTile.getText() == "") {
+            if (ninthTile.getText() == "" && !winnerCheck) {
                 tileClicked(ninthTile, playerTurn)
-                playerTurn.setText(changePlayerTurn(playerTurn))
+                winnerCheck = checkingForWinner(firstTile, secondTile, thirdTile, fourthTile, fifthTile,
+                    sixthTile, seventhTile, eighthTile, ninthTile)
+                if (winnerCheck){
+                    playerTurn.setText(winnerMet(playerTurn))
+                }
+                else if (!winnerCheck){
+                    playerTurn.setText(changePlayerTurn(playerTurn))
+                }
             }
         }
 
-        // The new game button
+        // The new game button will change all tiles to blank, player x will have the first turn,
+        // and set winnerCheck to false
         newGame.setOnClickListener {
             firstTile.setText("")
             secondTile.setText("")
@@ -103,6 +170,7 @@ class MainActivity : AppCompatActivity() {
             eighthTile.setText("")
             ninthTile.setText("")
             playerTurn.setText("Player X\'s turn")
+            winnerCheck = false
         }
     }
 
@@ -127,4 +195,46 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    // The function will take all of the tiles and check for any symbols lined up in 3 in row, return true
+    // if a row has been matched
+    fun checkingForWinner(tile1: Button, tile2: Button, tile3: Button, tile4: Button, tile5: Button,
+                          tile6: Button, tile7: Button, tile8: Button, tile9: Button):Boolean {
+        // Checking horizontal rows with symbols Xs or Os
+        if (((tile1.getText() == "X" || tile1.getText() == "O") && (tile2.getText() == "X" || tile2.getText() == "O") &&
+                    (tile3.getText() == "X" || tile3.getText() == "O")) || ((tile4.getText() == "X" || tile4.getText() == "O") &&
+                    (tile5.getText() == "X" || tile5.getText() == "O") && (tile6.getText() == "X" || tile6.getText() == "O")) ||
+            ((tile7.getText() == "X" || tile7.getText() == "O") && (tile8.getText() == "X" || tile8.getText() == "O")
+                    && (tile9.getText() == "X" || tile9.getText() == "O"))
+        ) {
+            return true
+        }
+        // Checking vertical rows with symbols Xs or Os
+        else if (((tile1.getText() == "X" || tile1.getText() == "O") && (tile4.getText() == "X" || tile4.getText() == "O") &&
+                    (tile7.getText() == "X" || tile7.getText() == "O")) || ((tile2.getText() == "X" || tile2.getText() == "O") &&
+                    (tile5.getText() == "X" || tile5.getText() == "O") && (tile8.getText() == "X" || tile8.getText() == "O")) ||
+            ((tile3.getText() == "X" || tile3.getText() == "O") && (tile6.getText() == "X" || tile6.getText() == "O")
+                    && (tile9.getText() == "X" || tile9.getText() == "O"))
+        ) {
+            return true
+        }
+        // Checking crossing rows with symbols Xs or 0s
+        else if (((tile1.getText() == "X" || tile1.getText() == "O") && (tile5.getText() == "X" || tile5.getText() == "O") &&
+                    (tile9.getText() == "X" || tile9.getText() == "O")) || ((tile7.getText() == "X" || tile7.getText() == "O") &&
+                    (tile5.getText() == "X" || tile5.getText() == "O") && (tile3.getText() == "X" || tile3.getText() == "O"))
+        ) {
+            return true
+        }
+        // Return false if none of the conditions are met
+        return false
+    }
+
+    // Change the playerTurn text to winner text
+    fun winnerMet(player:TextView):String{
+        if (player.getText() == "Player X\'s turn"){
+            return "X wins"
+        }
+        else {
+            return "O wins"
+        }
+    }
 }
